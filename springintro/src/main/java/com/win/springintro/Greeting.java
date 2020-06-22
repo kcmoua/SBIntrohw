adc59ac0f4db1983cbe8a7eb52a4200205f1c9d4
Keng Moua <keng@Kengs-MacBook-Pro.local>
2020-06-22 01:43:59 -0400

    first commit

---
+++ springintro/src/main/java/com/win/springintro/Greeting.java
@@ -0,0 +1,31 @@
+package com.win.springintro;
+
+public class Greeting {
+
+    private String firstName;
+    private String lastName;
+  
+    Greeting() {}
+
+    Greeting(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
---
+++ springintro/src/main/java/com/win/springintro/GreetingController.java
@@ -0,0 +1,23 @@
+package com.win.springintro;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String nameSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
+    }
+
+}
